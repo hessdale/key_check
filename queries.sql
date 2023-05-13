387021d1bf104b5d3be4f20a78b3c90cde041ddd
dale <hessdale96@gmail.com>
2023-05-13 14:21:01 -0600

    added queries.sql

---
+++ queries.sql
@@ -0,0 +1,3 @@
+insert into movie (title,runtime,director,release_year) values('Kill Bill',111,'Quentin Tarantino',20030910);
+insert into movie (title,runtime,director,release_year) values('Kill Bill: Volume 2',137,'Quentin Tarantino',20040508);
+insert into movie (title,runtime,director,release_year) values('Scary Movie',88,'Keenen Ivory Wayans',20000707);
