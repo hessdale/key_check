912412a400468ea5d3e47011eaa8483fbfb0902e
dale <hessdale96@gmail.com>
2023-05-13 14:46:22 -0600

    added unique key

--- dump-key_check-202305131445.sql
+++ dump-key_check-202305131445.sql
@@ -28,7 +28,8 @@ CREATE TABLE `movie` (
   `runtime` decimal(10,0) DEFAULT NULL,
   `director` varchar(30) DEFAULT NULL,
   `release_year` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `movie_un` (`title`,`director`,`release_year`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -55,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-13 14:17:16
+-- Dump completed on 2023-05-13 14:45:22
